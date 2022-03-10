0718ef92a32d40a974687c84ae862ed94936dccc
Stephen Gold <sgold@sonic.net>
2022-03-10 13:12:23 -0800

    FC2D:  avoid OpenGL "compatibility" profile (for portability)

--- FC2D/src/main/java/com/github/stephengold/fuzecreek/dd/FC2D.java
+++ FC2D/src/main/java/com/github/stephengold/fuzecreek/dd/FC2D.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2021, Stephen Gold
+ Copyright (c) 2021-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -189,6 +189,7 @@ public class FC2D
 
         AppSettings appSettings = new AppSettings(true);
         appSettings.setGammaCorrection(true);
+        appSettings.setRenderer(AppSettings.LWJGL_OPENGL32);
         appSettings.setResolution(1280, 800);
         appSettings.setVSync(true);
 
